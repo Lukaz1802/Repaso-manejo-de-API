398c76ba6c72e8b761c70d161740d12fb741c200
Lukaz1802 <lukaz1802.ll@gmail.com>
2021-12-13 00:55:01 -0300

    primer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+
+
+//////////////////////////////////////////////////////////////////
+
+//uso de fetch
+fetch('url')
+.then(res => res.json())
+.then(data => {
+  console.log(data)
+})
+
+
+//uso de async await
+const obtenerDatos = async() => {
+try {
+  const res = await fetch ('url')
+  const data = await res.json()
+  console.log(data)
+}catch (error) {
+  console.log(error)
+  }
+}
+obtenerDatos()
+
+
+////////////////////////////////////////////////////////////////
 
 function App() {
+
+const [equipo, setEquipo] = useState([])
+
+useEffect(() => {
+  obtenerDatos()
+  
+}, [])
+
+const obtenerDatos = async () => {
+ const data = await fetch('https://jsonplaceholder.typicode.com/users')
+ const users = await data.json()
+//  console.log(users)
+setEquipo(users)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Listando datos de API</h1>
+
+        <ul>
+          {
+            equipo.map(item => (
+              <li key={item.id}>{item.name} - {item.email}</li>
+            ))
+          }
+          </ul>      
     </div>
   );
 }
